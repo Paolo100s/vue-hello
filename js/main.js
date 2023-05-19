744ffee0b6e9b5d691ab77bb7f6f688f83476e02
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-05-19 13:22:57 +0200

    add on/off button

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@ createApp({
         return {
             title: "Ananas",
             myClasses: "text-orange bg-black",
-            image: "img/Ananas.jfif"
+            image: "img/Ananas.jfif",
+            otherClass: "on"
+        }
+    },
+    methods: {
+        disappear: function () {
+            if (this.otherClass === "on") {
+                this.otherClass = "hidden";
+            }
+            else {
+                this.otherClass = "on";
+            }
         }
     }
 }).mount('#myApp');
